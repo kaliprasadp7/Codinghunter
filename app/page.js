aab26ea707bc194857a18ab40880970f64636947
Kali Prasad Panda <kaliprasadp7@gmail.com>
2025-10-21 23:17:37 +0530

    pages router deleted and new app router added which is the latest next.js version and recomended and maintained by Next also the Navbar added for all pages using Layout.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,9 @@
 import Image from "next/image";
-import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
   return (
-    <div className="font-sans">
-      <head>
-        <title>CodingHunter</title>
-        <meta name="description" content="Your ultimate platform for coding challenges, tutorials, and resources to enhance your programming skills." />
-        
-      </head>
-      <Navbar />
+    <>
       <main className="flex flex-col gap-[32px] items-center justify-center px-24 py-18">
         
         <h1 className="text-5xl font-bold text-center sm:text-left"> Welcome to CodingHunter</h1>
@@ -19,6 +12,6 @@ export default function Home() {
         </p>
       </main>
 
-    </div>
+    </>
   );
 }
