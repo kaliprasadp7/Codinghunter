69c9b6553da632ec2666709109496cff69ab577f
Kali Prasad Panda <kaliprasadp7@gmail.com>
2025-10-21 14:54:15 +0530

    initial commit 2

--- app/page.js
+++ app/page.js
@@ -5,13 +5,13 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <head>
         <title>CodingHunter</title>
+        <meta name="description" content="Your ultimate platform for coding challenges, tutorials, and resources to enhance your programming skills." />
       </head>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-5xl font-bold text-center sm:text-left">  Welcome to CodingHunter</h1>
         <p className="text-lg text-center sm:text-left max-w-2xl">
           Your ultimate platform for coding challenges, tutorials, and resources to enhance your programming skills. Join our community of developers and start coding today!
         </p>
-
       </main>
 
     </div>
